db7dadfeccffb350c44eee8d557832eceab4f1e1
LmOpe <lawalmuhammed44@gmail.com>
2023-09-22 04:02:33 +0100

    Add features to allow students to access the course contents.

--- educa/students/views.py
+++ educa/students/views.py
@@ -1,4 +1,6 @@
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,6 +8,7 @@ from django.contrib.auth import authenticate, login
 
 
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 # Create your views here.
@@ -36,3 +39,33 @@ class StudentEnrollCourseView(LoginRequiredMixin,
     def get_success_url(self):
         return reverse_lazy('student_course_detail',
                             args=[self.course.id])
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get Course object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # Get current module
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id'])
+        else:
+            # Get first module
+            context['module'] = course.modules.all()[0]
+        
+        return context
