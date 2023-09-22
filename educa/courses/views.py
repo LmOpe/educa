fe52a288900c481a5475edba62aa75c82e1d08d6
LmOpe <lawalmuhammed44@gmail.com>
2023-09-22 03:33:59 +0100

    Implement student registration and enrollment functionalities.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -17,6 +17,7 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 from .forms import ModuleFormSet
 from .models import Course, Content, Module, Subject
+from students.forms import CourseEnrollForm
 
 # Create your views here.
 
@@ -196,3 +197,8 @@ class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+                                initial={'course':self.object})
+        return context
