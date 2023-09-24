74cbf88a85589774673498e673a76509b312148a
LmOpe <lawalmuhammedope@gmail.com>
2023-09-24 21:39:54 +0100

    Implement the WebSocket client

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from cannels.auth import AuthMiddlewareStack
+from channels.auth import AuthMiddlewareStack
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
